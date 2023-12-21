ee26fe406abf21eb9e1c2e0c7e56caa69265acd4
milagrosr <milagros.r23@gmail.com>
2023-12-20 23:10:23 -0300

    modificaciones nuevas

--- proyecto_reactVite_UTN_2023/vite.config.js
+++ proyecto_reactVite_UTN_2023/vite.config.js
@@ -1,6 +1,8 @@
-import ReactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  plugins: [ReactRefresh()],
-}
+export default defineConfig({
+  plugins: [react()],
+})
+
