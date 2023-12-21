09af5f343ba5be35127accba38de4e496faaa04c
milagrosr <milagros.r23@gmail.com>
2023-12-20 23:43:40 -0300

    modificaciones para ver la pagina

--- proyecto_reactVite_UTN_2023/vite.config.js
+++ proyecto_reactVite_UTN_2023/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://milir23.github.io/TPFINAL1_ReactVite_UTN_2023'
+
 })
